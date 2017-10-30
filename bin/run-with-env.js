2b712fc3be06afcdb8588e250d9355ac587fd0c5
bspot <con@bspot.de>
2017-10-30 14:11:59 +0100

    Set up basic flow: Parse command line -> collect environment -> run command.

--- bin/run-with-env.js
+++ bin/run-with-env.js
@@ -1,1 +1,24 @@
 #!/usr/bin/env node
+const parseCommandLine = require('../lib/parseCommandLine')
+const compileEnvironment = require('../lib/compileEnvironment')
+const execCommand = require('../lib/execCommand')
+
+const main = () => Promise.resolve().then(() => {
+  const { actions, command } = parseCommandLine(process.argv.slice(2))
+
+  const env = compileEnvironment(process.env, actions)
+
+  if (command) {
+    return execCommand(command, env).then(({ code }) => code)
+  }
+  else {
+    console.log(JSON.stringify(env, null, 2))
+  }
+})
+
+main()
+  .then((code) => process.exit(code || 0))
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
